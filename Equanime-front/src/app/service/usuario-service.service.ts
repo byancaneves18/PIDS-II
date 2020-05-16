ba0a61691e803d8a87dbbc7ff597657b901325ec
Gabriel <gabriel.nunes.melo1@gmail.com>
2020-05-16 18:51:40 -0300

    Auto stash before merge of "Crud-Disciplinas" and "origin/Crud-Disciplinas"

--- Equanime-front/src/app/service/usuario-service.service.ts
+++ Equanime-front/src/app/service/usuario-service.service.ts
@@ -1,8 +1,8 @@
-
+import { Usuarios } from 'src/app/modelo/usuario.modelo';
 import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import { HttpClient} from '@angular/common/http';
-import { Usuarios } from '../modelo/usuario.modelo';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +14,28 @@ export class UsuarioServiceService {
 
   constructor(private http: HttpClient) { }
 
+  getUsuarios(): Observable<Usuarios[]> {
+    return this.http.get<Usuarios[]>(this.URL_API + '/listar');
+  }
 
-    getUsuarios(): Observable<Usuarios[]> {
-      return this.http.get<Usuarios[]>(this.URL_API + '/listar');
-    }
+  cadastrarUsuario(formulario: Usuarios) {
+    return this.http.post(this.URL_API + '/salvar', formulario);
+  }
 
+  getUsuario(id: string): Observable<Usuarios> {
+    return this.http.get<Usuarios>(this.URL_API + '/id');
+  }
+
+  atualizar(id: string, request: any) {
+    return this.http.put(this.URL_API, request);
+  }
+
+  deletarUsuario(usuario: Usuarios) {
+    this.getUsuarios();
+    return this.http.post(this.URL_API + '/deletar', usuario);
+  }
+
+  buscarUsuario(nome: String) {
+    return this.http.post(this.URL_API + '/buscar', nome);
+  }
 }
